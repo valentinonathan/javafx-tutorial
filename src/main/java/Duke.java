5f04f5ebbbbd3d7e07573e1ce512c5c77144ecd0
Valentino Nathan <nathan070907@gmail.com>
2026-09-06 23:18:25 +0800

    Add chat UI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Application.launch(Main.class, args);
     }
 }
